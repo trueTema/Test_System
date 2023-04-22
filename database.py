3c39b0f665c7f726c2bbc3b8f33790348f2063c0
Georgii <tnnkrch@mail.ru>
2023-04-22 21:03:48 +0300

    Добавлена проверка на целочисленный формат айди

--- database.py
+++ database.py
@@ -17,7 +17,7 @@ def add_parcel(parcel: Parcel, connection: sqlite3.Connection):
                     f"values({parcel.id_task}, {parcel.id_user}, {parcel.points}, \"{parcel.answer}\", {parcel.date});")
         connection.commit()
     except Exception as e:
-        print(f'[Error] While adding a new parcel to database {e} has occured.')
+        print(f'[Error] While adding a new parcel to database {e} has occurred.')
     finally:
         cur.close()
 
@@ -37,7 +37,7 @@ def update_parcel(parcel: Parcel, connection: sqlite3.Connection):
                     f"where student_ID = {parcel.id_user} and problem_ID = {parcel.id_task};")
         connection.commit()
     except Exception as e:
-        print(f'[Error] While updating a parcel in database {e} has occured.')
+        print(f'[Error] While updating a parcel in database {e} has occurred.')
     finally:
         cur.close()
 
@@ -54,7 +54,7 @@ def delete_parcel(parcel: Parcel, connection: sqlite3.Connection):
                     f"where problem_ID = {parcel.id_task} and student_ID = {parcel.id_user};")
         connection.commit()
     except Exception as e:
-        print(f'[Error] While deleting a parcel from database {e} has occured.')
+        print(f'[Error] While deleting a parcel from database {e} has occurred.')
     finally:
         cur.close()
 
@@ -71,7 +71,7 @@ def add_problem(problem: TASK, connection: sqlite3.Connection):
                     f"values({problem.id}, {problem.id_of_user}, \"{problem.statement}\", {problem.visible}, \"{problem.group}\");")
         connection.commit()
     except Exception as e:
-        print(f'[Error] While adding a new problem {e} has occured.')
+        print(f'[Error] While adding a new problem {e} has occurred.')
     finally:
         cur.close()
 
@@ -92,7 +92,7 @@ def get_problem(id: int, connection: sqlite3.Connection) -> Task.TASK or None:
         res_task = TASK(id=res[0][0], visible=res[0][3], id_of_user=res[0][1], statement=res[0][2], group=res[0][4])
         return res_task
     except Exception as e:
-        print(f'[Error] While getting a problem from database {e} has occured.')
+        print(f'[Error] While getting a problem from database {e} has occurred.')
     finally:
         cur.close()
 
@@ -112,7 +112,7 @@ def update_problem(problem: TASK, connection: sqlite3.Connection):
                     f"where problem_ID = {problem.id};")
         connection.commit()
     except Exception as e:
-        print(f'[Error] While updating a problem in database {e} has occured.')
+        print(f'[Error] While updating a problem in database {e} has occurred.')
     finally:
         cur.close()
 
@@ -131,7 +131,7 @@ def delete_problem(problem_id: int, connection: sqlite3.Connection):
                     f"where problem_ID = {problem_id}")
         connection.commit()
     except Exception as e:
-        print(f'[Error] While deleting an user from database {e} has occured.')
+        print(f'[Error] While deleting an user from database {e} has occurred.')
     finally:
         cur.close()
 
@@ -148,7 +148,7 @@ def add_user(user: users.User, connection: sqlite3.Connection):
                     f"values({user.id}, \"{user.name}\", \"{user.status}\", \"{user.study_group}\");")
         connection.commit()
     except Exception as e:
-        print(f'[Error] While adding a new parcel to database {e} has occured.')
+        print(f'[Error] While adding a new parcel to database {e} has occurred.')
     finally:
         cur.close()
 
@@ -168,7 +168,7 @@ def update_user_info(user: users.User, connection: sqlite3.Connection):
                     f"where id = {user.id};")
         connection.commit()
     except Exception as e:
-        print(f'[Error] While adding a new parcel to database {e} has occured.')
+        print(f'[Error] While adding a new parcel to database {e} has occurred.')
     finally:
         cur.close()
 
@@ -189,7 +189,7 @@ def get_user(id: int, connection: sqlite3.Connection) -> users.User or None:
         result = users.User(id=res[0][0], name=res[0][1], status=res[0][2], study_group=res[0][3])
         return result
     except Exception as e:
-        print(f'[Error] While getting an user from database {e} has occured.')
+        print(f'[Error] While getting an user from database {e} has occurred.')
     finally:
         cur.close()
 
@@ -206,7 +206,7 @@ def del_user(user: users.User, connection: sqlite3.Connection):
                     f"where id = {user.id};")
         connection.commit()
     except Exception as e:
-        print(f'[Error] While deleting an user from database {e} has occured.')
+        print(f'[Error] While deleting an user from database {e} has occurred.')
     finally:
         cur.close()
 
@@ -229,7 +229,7 @@ def get_user_parcels(user_id: int, connection: sqlite3.Connection) -> list:
         res = list(map(lambda x: Parcel(id_user=x[1], id_task=x[0], points=x[2], answer=x[3], date=x[4]), res))
         return res
     except Exception as e:
-        print(f'[Error] While searching for user\'s parcels in database {e} has occured.')
+        print(f'[Error] While searching for user\'s parcels in database {e} has occurred.')
     finally:
         cur.close()
 
@@ -253,7 +253,7 @@ def get_user_problem_parcels(user_id: int, problem_id: int, connection: sqlite3.
         res = list(map(lambda x: Parcel(id_user=x[1], id_task=x[0], points=x[2], answer=x[3], date=x[4]), res))
         return res
     except Exception as e:
-        print(f'[Error] While searching for user\'s parcels in database {e} has occured.')
+        print(f'[Error] While searching for user\'s parcels in database {e} has occurred.')
     finally:
         cur.close()
 
@@ -275,6 +275,6 @@ def get_problem_parcels(problem_id: int, connection: sqlite3.Connection) -> list
         res = list(map(lambda x: Parcel(id_user=x[1], id_task=x[0], points=x[2], answer=x[3], date=x[4]), res))
         return res
     except Exception as e:
-        print(f'[Error] While searching for problem\'s parcels in database {e} has occured.')
+        print(f'[Error] While searching for problem\'s parcels in database {e} has occurred.')
     finally:
         cur.close()
